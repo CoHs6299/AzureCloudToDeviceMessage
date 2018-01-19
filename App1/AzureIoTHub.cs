ed1e590804e9590a9f1185f899d217f766f51c8a
Tommy Wong <cohs6299@hotmail.com>
2018-01-20 01:23:00 +0800

    Azure C2D

---
+++ App1/AzureIoTHub.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+
+class AzureIoTHub
+{
+    private static void CreateClient()
+    {
+        if (deviceClient == null)
+        {
+            deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+        }
+    }
+
+    static DeviceClient deviceClient = null;
+    const string deviceConnectionString = "HostName=ProjectYsIoTHub.azure-devices.net;DeviceId=HeyHiHello;SharedAccessKey=zjH76Z3cifItfY6WSWujn/Dp0V7kCIMostBpp7vWoWw=";
+
+    public static async Task SendDeviceToCloudMessageAsync()
+    {
+        CreateClient();
+#if WINDOWS_UWP
+        var str = "{\"deviceId\":\"HeyHiHello\",\"messageId\":1,\"text\":\"Hello, Cloud from a UWP C# app!\"}";
+#else
+        var str = "{\"deviceId\":\"HeyHiHello\",\"messageId\":1,\"text\":\"Hello, Cloud from a C# app!\"}";
+#endif
+        var message = new Message(Encoding.ASCII.GetBytes(str));
+
+        await deviceClient.SendEventAsync(message);
+    }
+
+    public static async Task<string> ReceiveCloudToDeviceMessageAsync()
+    {
+        CreateClient();
+
+        while (true)
+        {
+            var receivedMessage = await deviceClient.ReceiveAsync();
+
+            if (receivedMessage != null)
+            {
+                var messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                await deviceClient.CompleteAsync(receivedMessage);
+                return messageData;
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Azure.Devices;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+
+namespace App1
+{
+
+    public sealed partial class MainPage : Page
+    {
+        const string connectionString = "HostName=ProjectYsIoTHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=koDZxijqb/4/COCRlSFc2V6J869NVh1gl4JIP/0atPo=";
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Task.Run(
+                    async () => {
+                        while (true)
+                        {
+                            var message = await AzureIoTHub.ReceiveCloudToDeviceMessageAsync();
+                            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.High, () => {
+                                test.Text += Environment.NewLine + message;
+                            });
+                        }
+                    }
+                    );
+        }
+        private static async Task SendCloudToDeviceMessageAsync()
+        {
+            var serviceClient = ServiceClient.CreateFromConnectionString(connectionString, TransportType.Amqp);
+            var message = new Message(System.Text.Encoding.ASCII.GetBytes("finally"));
+            await serviceClient.SendAsync("HeyHiHello", message);
+        }
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await SendCloudToDeviceMessageAsync();
+        }
+    }
+}
